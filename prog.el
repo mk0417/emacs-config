04bba9f8a9a82295f66598b35c8d208a2dd31885
“mk0417” <pengdata1@gmail.com>
2020-11-10 14:54:03 +0000

    fix python and jupyter

--- prog.el
+++ prog.el
@@ -4,26 +4,32 @@
 
 (with-eval-after-load 'python
   (add-hook 'python-mode-hook 'display-fill-column-indicator-mode)
-  ;; jupyter
-  (setq jupyter-eval-use-overlays t)
-  ;; keybindings
-  (general-create-definer p-python-leader-normal-def
-    :prefix ";"
-    :states 'normal
-    :keymaps 'python-mode-map)
-  (p-python-leader-normal-def
-   "jj" 'jupyter-run-repl
-   "jf" 'jupyter-eval-defun
-   "jr" 'jupyter-eval-line-or-region
-   "jR" 'jupyter-repl-restart-kernel
-   "jC" 'jupyter-repl-clear-cells
-   "jI" 'jupyter-interrupt-kernel
-   "ji" 'jupyter-inspect-at-point
-   "jc" 'jupyter-eval-remove-overlays)
-  (general-create-definer p-python-leader-visual-def
-    :prefix ";"
-    :states 'visual
-    :keymaps 'python-mode-map)
-  (p-python-leader-normal-def
-   "jr" 'jupyter-eval-line-or-region))
+  ;; disable warning
+  (setq python-indent-guess-indent-offset-verbose nil
+        python-indent-guess-indent-offset nil))
+
+;; jupyter
+(setq jupyter-eval-use-overlays t)
+
+;; keybindings
+(general-create-definer p-python-leader-normal-def
+  :prefix ";"
+  :states 'normal
+  :keymaps 'python-mode-map)
+(p-python-leader-normal-def
+  "jj" 'jupyter-run-repl
+  "jf" 'jupyter-eval-defun
+  "jr" 'jupyter-eval-line-or-region
+  "jR" 'jupyter-repl-restart-kernel
+  "jC" 'jupyter-repl-clear-cells
+  "jI" 'jupyter-interrupt-kernel
+  "ji" 'jupyter-inspect-at-point
+  "jc" 'jupyter-eval-remove-overlays)
+
+(general-create-definer p-python-leader-visual-def
+  :prefix ";"
+  :states 'visual
+  :keymaps 'python-mode-map)
+(p-python-leader-visual-def
+  "jr" 'jupyter-eval-line-or-region)
 
